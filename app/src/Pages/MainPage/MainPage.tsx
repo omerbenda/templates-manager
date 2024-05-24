613ac075a745799685772acbfbece0af14fe3c4c
omerbenda <34948058+omerbenda@users.noreply.github.com>
2024-05-24 16:31:02 +0300

    wip(MainPage.tsx): Add test data for file system display

--- app/src/Pages/MainPage/MainPage.tsx
+++ app/src/Pages/MainPage/MainPage.tsx
@@ -1,5 +1,31 @@
 const MainPage = () => {
-  return <div className="text-3xl">Main Page</div>;
+  const dirGap = 100;
+
+  return (
+    <div className="w-full">
+      <div
+        className="flex flex-col items-end"
+        style={{ width: `calc(100% - ${dirGap}px)` }}
+      >
+        <div className="w-full text-2xl">Dir</div>
+        <div
+          className="flex flex-col items-end"
+          style={{ width: `calc(100% - ${dirGap}px)` }}
+        >
+          <div className="w-full text-2xl">Dir</div>
+          <div
+            className="flex flex-col items-end"
+            style={{ width: `calc(100% - ${dirGap}px)` }}
+          >
+            <div className="w-full text-2xl">File1</div>
+            <div className="w-full text-2xl">File2</div>
+          </div>
+          <div className="w-full text-2xl">File1</div>
+          <div className="w-full text-2xl">File2</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MainPage;
