4c744f5808c22092ee3083c664a27f40e5e3364f
omerbenda <34948058+omerbenda@users.noreply.github.com>
2024-05-25 11:37:07 +0300

    feat(Directory.tsx): Add opening and closing directory

--- app/src/Pages/MainPage/Components/Directory/Directory.tsx
+++ app/src/Pages/MainPage/Components/Directory/Directory.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import DirectoryData from '../../Types/DirectoryData';
 import FileData from '../../Types/FileData';
 
@@ -8,19 +9,28 @@ type Props = {
 };
 
 const Directory = ({ dirData }: Props) => {
+  const [isOpen, setOpen] = useState<boolean>(false);
+
   return (
     <div className="flex flex-col items-end w-full">
-      <div className="w-full text-2xl">{dirData.name}</div>
-      <div style={{ width: `calc(100% - ${WIDTH_GAP})` }}>
-        {dirData.subdirs.map((subdir: DirectoryData) => (
-          <Directory dirData={subdir} key={subdir.name} />
-        ))}
-        {dirData.files.map((file: FileData) => (
-          <div className="w-full text-2xl" key={file.name}>
-            {file.name}
-          </div>
-        ))}
+      <div
+        onClick={() => setOpen((curr) => !curr)}
+        className="select-none cursor-pointer w-full text-2xl"
+      >
+        {dirData.name}
       </div>
+      {isOpen && (
+        <div style={{ width: `calc(100% - ${WIDTH_GAP})` }}>
+          {dirData.subdirs.map((subdir: DirectoryData) => (
+            <Directory dirData={subdir} key={subdir.name} />
+          ))}
+          {dirData.files.map((file: FileData) => (
+            <div className="select-none w-full text-2xl" key={file.name}>
+              {file.name}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
