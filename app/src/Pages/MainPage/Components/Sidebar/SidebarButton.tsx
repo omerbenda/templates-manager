185b764c86f2409eb5c4c65970db5726884f9799
omerbenda <34948058+omerbenda@users.noreply.github.com>
2024-05-29 21:26:33 +0300

    feat(SidebarButton.tsx): Add pressing animation to sidebar buttons

--- app/src/Pages/MainPage/Components/Sidebar/SidebarButton.tsx
+++ app/src/Pages/MainPage/Components/Sidebar/SidebarButton.tsx
@@ -13,8 +13,8 @@ const SidebarButton = ({ children, onClick }: Props) => {
         min-h-16
         cursor-pointer select-none
         duration-300
-        border-b-2 border-gray-400
-        bg-gray-200 hover:bg-gray-300
+        border-b-2 border-gray-500
+        bg-gray-200 hover:bg-gray-300 active:bg-gray-400
       "
     >
       {children}
